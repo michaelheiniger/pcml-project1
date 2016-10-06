90330462c3a9a1ac32b7f1cee1a0f33d77e5b457
Michael Heiniger <michael.heiniger@epfl.ch>
2016-10-06 11:07:49 +0200

    Add provided code and regression_functions containing the six regression functions to be implemented.

---
+++ python/regression_functions.py
@@ -0,0 +1,32 @@
+""" Functions implementing different regression models."""
+
+import numpy as np
+
+def least_squares_GD(y, tx, gamma, max_iters):
+    #TODO Chiara
+    raise NotImplementedError
+
+def least_squares_SGD(y, tx, gamma, max_iters):
+    # TODO Chiara
+    raise NotImplementedError
+
+def least_squares(y, tx):
+    # TODO Sandro
+    raise NotImplementedError
+
+def ridge_regression(y, tx, lambda_):
+    # TODO Sandro
+    raise NotImplementedError
+
+def logistic_regression(y, tx, gamma, max_iters):
+    # TODO Michael
+    raise NotImplementedError
+
+def reg_logistic_regression(y, tx, lambda_, gamma, max_iters):
+    # TODO Michael
+    raise NotImplementedError
+
+
+
+
+
