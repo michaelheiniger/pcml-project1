dcaa5b1d9d416751aee189d158dfe3128d5b90c2
Chiara Orvati <chiara.orvati@epfl.ch>
2016-10-11 11:19:50 +0200

    fill in the first 2 functions

--- python/regression_functions.py
+++ python/regression_functions.py
@@ -1,14 +1,38 @@
 """ Functions implementing different regression models."""
 
 import numpy as np
+import gradient_descent as gd
+import stochastic_gradient_descent as sgd
 
 def least_squares_GD(y, tx, gamma, max_iters):
-    #TODO Chiara
-    raise NotImplementedError
+    """ Linear Regression using Gradient Descent"""
+    
+    # initialization of the weights with zeros
+    w_initial = np.array(np.zeros(tx.shape[1]))
+    # get the losses and weights for every iteration
+    gd_losses, gd_ws = gd.gradient_descent(y, tx, w_initial, max_iters, gamma)
+    # take the best values
+    opt_weights = gd_ws[max_iters-1]
+    opt_loss = gd_losses[max_iters-1]
+    
+    return opt_weights, opt_loss
+
+
 
 def least_squares_SGD(y, tx, gamma, max_iters):
-    # TODO Chiara
-    raise NotImplementedError
+    """Linear Regression using Stochastic Gradient Descent"""
+    
+    # initalization of the weights with zeros
+    w_initial = np.array(np.zeros(tx.shape[1]))
+    # get the losses and weights for every iteration
+    sgd_losses, sgd_ws = sgd.stochastic_gradient_descent(y, tx, w_initial, batch_size, max_iters, gamma)
+    # take the best values
+    opt_weights = sgd_ws[max_iters-1]
+    opt_loss = sgd_losses[max_iters-1]
+    
+    return opt_weights, opt_loss
+
+
 
 def least_squares(y, tx):
     # TODO Sandro
