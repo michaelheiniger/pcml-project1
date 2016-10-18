1ad1ae9d8aa97d6ae79232f4a1c4a972beb7daac
Chiara Orvati <chiara.orvati@epfl.ch>
2016-10-18 18:56:26 +0200

    added build_poly for matrices

--- python/build_polynomial.py
+++ python/build_polynomial.py
@@ -6,7 +6,22 @@ import numpy as np
 
 def build_poly(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
-    phi = np.array(np.zeros((x.shape[0], degree+1)))
+    phi = np.zeros((x.shape[0], degree+1))
     for d in range(0,degree+1):
         phi[:, d] = np.power(x,d)
+    return phi
+
+def build_poly_matrix(tx, degree):
+    """polynomial basis function for input data x, 
+    the returned matrix is of the form [x^0 x^1 ... x^(degree)]"""
+    rows = tx.shape[0]
+    cols = int(tx.size / rows)
+    dummy = np.copy(tx)
+    
+    if cols == 1: #add a second dimension to 1D array
+        dummy = np.reshape(dummy, (rows,1)) 
+    #fill phi with the powers of tx
+    phi = np.empty((rows, cols * (degree + 1)))
+    for d in range(0, degree +1):
+        phi[:, d*cols: (d+1) * cols] = np.power(dummy, d)
     return phi
