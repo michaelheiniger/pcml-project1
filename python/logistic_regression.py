63445c05a872af248c0b84ce4dd9c02b3d887f35
Chiara Orvati <chiara.orvati@epfl.ch>
2016-10-26 11:16:53 +0200

    prevent overflow

--- python/logistic_regression.py
+++ python/logistic_regression.py
@@ -2,39 +2,59 @@ import numpy as np
 from helpers import batch_iter
 
 def sigmoid(t):
-    return np.divide(1, np.add(1, np.exp(-t)))
+    result = np.empty(np.shape(t))
+    id_pos = t > 100
+    id_neg = t < -100
+    id_rest = np.logical_and(-100 <= t , t <= 100)
+    #prevent over/underflow by assigning the limits of the sigmoid for large values of t
+    result[id_pos] = 1.
+    result[id_neg] = 0.
+    result[id_rest] = np.divide(1, np.add(1, np.exp(-t)))
+    return result
 
 def compute_log_likelihood(y, tx, w):
+    """computes the loss (negative log likelihood) for logistic regression"""
     loss = 0
     for i in range(tx.shape[0]):
         y_est = tx[i].dot(w)
-        loss = loss + np.log(1 + np.exp(y_est)) - y[i] * y_est
+        
+        # prevent overflow by assuming log(1+exp(k)) = k ,for large k
+        if y_est > 100: 
+            loss += y_est - y[i] * y_est
+        else:
+            loss +=  np.log(1 + np.exp(y_est)) - y[i] * y_est
     return loss
 
-def compute_log_likelihood(y, tx, w):
-    loss = 0
-    for i in range(tx.shape[0]):
-        y_est = tx[i].dot(w)
-        loss = loss + np.log(1 + np.exp(y_est)) - y[i] * y_est
-    return loss
 
 def compute_log_likelihood_penalized(y, tx, w, lambda_):
+    """computes the loss (negative log likelihood+ penalization term) for regularized logistic regression"""
+    
     loss = 0
     for i in range(tx.shape[0]):
         y_est = tx[i].dot(w)
-        loss = loss + np.log(1 + np.exp(y_est)) - y[i] * y_est
+        # prevent overflow by assuming log(1+exp(k)) = k ,for large k
+        if y_est > 100: 
+            loss += y_est - y[i] * y_est
+        else:
+            loss +=  np.log(1 + np.exp(y_est)) - y[i] * y_est
+    # add the penalization term to the loss
     return np.add(loss, lambda_ * w.transpose().dot(w))
+
 
 def compute_gradient_log_likelihood(y, tx, w):
     """Computes gradient of the max likelihood estimator for logistic regression"""
+    
     xt_t = tx.transpose()
     return xt_t.dot((sigmoid(tx.dot(w)) - y))
 
+
 def compute_gradient_log_likelihood_penalized(y, tx, w, lambda_):
     """Computes gradient of the max likelihood estimator for logistic regression"""
+    
     xt_t = tx.transpose()
     gradient = xt_t.dot((sigmoid(tx.dot(w)) - y))
     return np.add(gradient, np.multiply(2 * lambda_, w))
+
 
 def logistic_regression_SGD(y, tx, initial_w, batch_size, max_iters, gamma, lambda_ = 0):
     """ Logistic regression using Schocastic gradient descent algorithm """
